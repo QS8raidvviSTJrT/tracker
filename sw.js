cf7d546fecaa1789ea33241d6cee40ccaa23b5c5
Admin <sellxsolutions@gmail.com>
2025-05-23 21:35:44 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -25,7 +25,7 @@ var APP_PREFIX = 'dt';
 // you need to change this version (version_01, version_02…). 
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = 'version_06';
+var VERSION = 'version_07';
  
 // The files to make available for offline use. make sure to add 
 // others to this list
@@ -34,7 +34,26 @@ var URLS = [
   `${GHPATH}/index.html`,
   `${GHPATH}/styles.css`,
   `${GHPATH}/app.js`,
+  `${GHPATH}/map.js`,
+  `${GHPATH}/manifest.json`,
   `${GHPATH}/images/180.png`,
   `${GHPATH}/images/512.png`,
-  `${GHPATH}/images/favicon.ico`
+  `${GHPATH}/images/favicon.ico`,
+  `${GHPATH}/images/favicon-16x16.png`,
+  `${GHPATH}/images/favicon-32x32.png`,
+  `${GHPATH}/images/favicon-96x96.png`,
+  `${GHPATH}/images/favicon-128x128.png`,
+  `${GHPATH}/images/favicon-192x192.png`,
+  `${GHPATH}/images/favicon-512x512.png`,
+  `${GHPATH}/images/favicon-180.png`,
+  `${GHPATH}/images/favicon-512.png`,
+  `${GHPATH}/images/favicon-16.png`,
+  `${GHPATH}/images/favicon-32.png`,
+  `${GHPATH}/images/favicon-96.png`,
+  `${GHPATH}/images/favicon-128.png`,
+  `${GHPATH}/images/favicon-180.png`,
+  `${GHPATH}/images/favicon-512.png`,
+  `${GHPATH}/images/favicon-16.png`,
+  `${GHPATH}/images/favicon-32.png`,
+  `${GHPATH}/images/favicon-96.png`,
 ]
